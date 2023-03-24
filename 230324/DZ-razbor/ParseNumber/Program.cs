45a698d29edf02094491812562fe35dd9bcde1a0
Aleksey Burov <burovalexprogrammer@gmail.com>
2023-03-24 21:23:25 +0200

    Implement Parse Number

---
+++ 230324/DZ-razbor/ParseNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+string _line;
+StringBuilder _valueStringBuilder = new StringBuilder();
+float _value = 0f;
+
+//ввод
+Console.WriteLine("Vvedite stroky: ");
+_line = Console.ReadLine();
+
+//обработка
+ParseLine(true);
+
+if (_valueStringBuilder.Length == 0)
+{
+    Console.WriteLine("Error. Number not found");
+}
+else
+{
+    _value = float.Parse(_valueStringBuilder.ToString());
+}
+
+//вывод
+Console.WriteLine("Number = " + _value);
+
+void ParseLine(bool exitOnFirstLetter)
+{
+    for (int i = 0; i < _line.Length; i++)
+    {
+        char letter = _line[i];
+
+        if (char.IsDigit(letter))
+        {
+            _valueStringBuilder.Append(letter);
+            continue;
+        }
+        else if (letter == '.' || letter == ',')
+        {
+            _valueStringBuilder.Append(',');
+            continue;
+        }
+
+        if (exitOnFirstLetter) return; 
+    }
+}
