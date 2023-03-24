33e28689e6a3688f64e41982cbc60d75adc24df1
Aleksey Burov <burovalexprogrammer@gmail.com>
2023-03-24 20:55:02 +0200

    Razbor DZ 1

--- 230324/DZ-razbor/DZ1/Program.cs
+++ 230324/DZ-razbor/DZ1/Program.cs
@@ -1,57 +1,87 @@
-﻿namespace SN_HW_23._03._23;
+﻿
+
+namespace SN_HW_23._03._23;
 class Program
 {
+    enum Types
+    {
+        StringType,
+        NumberType
+    }
+
     static void Main(string[] args)
     {
-
-        //Вариант без методов
+        string _userValue;
+        float _value;
+        string valueType = "";
+        const string NumberType = "number";
+        const string StringType = "string";
 
         while (true)
         {
-            string _userValue;
-            float _value;
+            _userValue = ReadValue();
 
-            Console.Write("Введите значение: ");
-            _userValue = Console.ReadLine().Replace(",", ".");
+            if (_userValue == "exit") break;
 
-            if (_userValue == "exit")
+            valueType = GetValueType();
+
+            switch (valueType)
             {
-                break;
-            }
-
-            bool isNumber = float.TryParse(_userValue, out _value);
-
-            if (isNumber)
-            {
-                if (_value % 1 == 0)
-                {
-                    Console.WriteLine($"{_value} -  Целое число\n");
-                }
-                else
-                {
-                    Console.WriteLine($"{_value} -  Вещественное число\n");
-                }
-            }
-            else
-            {
-                char[] separators = new char[] { ' ', ',', '.' };
-                string[] words = _userValue.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-                int result = words.Length;
-
-                if (result == 1)
-                {
-                    Console.Write($"{_userValue} -  Слово.\t");
-                    Console.WriteLine($"Длина слова =  {_userValue.Length}\n");
-                }
-                else
-                {
-                    Console.Write($"{_userValue} -  Предложение.\t");
-                    Console.WriteLine($"Количество слов =  {result}\n");
-                }
+                case NumberType:
+                    PrintNumberInfo();
+                    break;
+                case StringType:
+                    PrintStringInfo();
+                    break;
             }
         }
 
         Console.ReadKey();
+
+
+
+        string ReadValue()
+        {
+            Console.Write("Введите значение: ");
+            return Console.ReadLine().Replace(".", ",");
+        }
+
+        string GetValueType()
+        {
+            bool isNumber = float.TryParse(_userValue, out _value);
+
+            return isNumber ? NumberType : StringType;
+        }
+
+        void PrintNumberInfo()
+        {
+            if (_value % 1 == 0)
+            {
+                Console.WriteLine($"{_value} -  Целое число\n");
+            }
+            else
+            {
+                Console.WriteLine($"{_value} -  Вещественное число\n");
+            }
+        }
+
+        void PrintStringInfo()
+        {
+            char[] separators = new char[] { ' ', ',', '.' };
+            string[] words = _userValue.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int wordCount = words.Length;
+
+            if (wordCount == 1)
+            {
+                Console.Write($"{_userValue} -  Слово.\t");
+                Console.WriteLine($"Длина слова =  {_userValue.Length}\n");
+            }
+            else
+            {
+                Console.Write($"{_userValue} -  Предложение.\t");
+                Console.WriteLine($"Количество слов =  {wordCount}\n");
+            }
+        }
     }
 }
 
