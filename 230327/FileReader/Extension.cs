c9b2c4655069c203360451d43981529f7d62e93a
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-03-27 17:37:39 +0200

    Add fileReader

---
+++ 230327/FileReader/Extension.cs
@@ -0,0 +1,21 @@
+﻿namespace FileReader
+{
+    public static class Extension
+    {
+        public static void PrintVar(this object variable, string description)
+        {
+            Console.WriteLine(description + ": " + variable.ToString());
+        }
+
+        //public static bool TryFindParentDirectory(this DirectoryInfo directory, string targetFolder, out DirectoryInfo directoryResult)
+        //{
+        //    string folderName = new DirectoryInfo(directory.FullName).Name;
+        //    bool compareResult = string.Compare(folderName, targetFolder, StringComparison.OrdinalIgnoreCase) == 0;
+        //    if (compareResult)
+        //    {
+        //        directoryResult = ;
+        //        return true;
+        //    } 
+        //}
+    }
+}
---
+++ 230327/FileReader/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FileReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _dataFileName = "data.txt";
+            var _filePath = "";
+            var _appPath = Directory.GetCurrentDirectory();
+            _appPath.PrintVar("bin  AppPath");
+            _appPath = new DirectoryInfo(_appPath).Parent.Parent.Parent.FullName;
+            _appPath.PrintVar("root AppPath");
+            _filePath = Path.Combine(_appPath, _dataFileName);
+            _filePath.PrintVar("file path");
+
+            ReadFile(_filePath);
+                
+            Console.ReadLine();
+        }
+
+        static void ReadFile(string path)
+        {
+            var text = "";
+            try
+            {
+                text = File.ReadAllText(path);
+                Console.WriteLine("Text: " + text);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Reading done.");
+            }
+        }
+    }
+}
